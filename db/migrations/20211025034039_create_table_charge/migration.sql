258518543ac0eb8f6d6cc5be959eeebbad4b7902
Joel Olivier Sidy <joeloliviersidy@gmail.com>
2021-10-25 06:41:07 +0300

    feat: create table charge

---
+++ db/migrations/20211025034039_create_table_charge/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Charge` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `designation` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `prix` INTEGER NOT NULL,
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `societeId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Charge` ADD FOREIGN KEY (`societeId`) REFERENCES `Societe`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
